11928c188a3036a24eec9e723772a191424a2aa5
Suman Saha <studio.suman@gmail.com>
2022-07-17 22:19:08 +0530

    Updated Stm32 deps

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# Stm32
+# **Stm32**
 
-Building STM32
+## _Building STM32_
+
+----
+- Awesome
+* STM32
+
+----
+
+```C++
+include <arduino.h>
+print(string.ascii_lowercase)
+
+```
+[Stm32 Page Link] https://github.com/studio-suman/Stm32.git
+
+
+**Note:** This is a work in progress.
---
+++ install_drivers.bat
@@ -0,0 +1,12 @@
+@echo off
+
+pause
+echo Installing Maple Serial driver...
+"%~dp0wdi-simple" --vid 0x1EAF --pid 0x0004 --type 3 --name "Maple Serial" --dest "%~dp0maple-serial"
+echo.
+
+pause
+
+echo Installing Maple DFU driver...
+"%~dp0wdi-simple" --vid 0x1EAF --pid 0x0003 --type 1 --name "Maple DFU" --dest "%~dp0maple-dfu"
+echo.
